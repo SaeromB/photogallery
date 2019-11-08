463be6174877f2517681c0794190466270cc3db3
Saerom <saerombang11@gmail.com>
2019-11-08 19:53:08 +0900

    first commit

---
+++ src/services/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBGB380jabr4Ylc26qNBD2Wd9tH6KKMTM",
+    authDomain: "photogallery-48c40.firebaseapp.com",
+    databaseURL: "https://photogallery-48c40.firebaseio.com",
+    projectId: "photogallery-48c40",
+    storageBucket: "photogallery-48c40.appspot.com",
+    messagingSenderId: "892646622249",
+    appId: "1:892646622249:web:0ee1f10db79d892dfa0838"
+  };
+
+  
